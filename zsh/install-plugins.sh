add786d61896c9f388534533a47a0e93f059a775
Luis Acosta <luis13cst@gmail.com>
2021-12-30 11:23:14 +0100

    feat: script for install zsh plugins

---
+++ zsh/install-plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install the plugins that are enabled in .zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
